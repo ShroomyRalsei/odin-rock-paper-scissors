fca457f920a3b0c7459c5658476dfef43f21cee2
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-13 19:46:11 -0400

    feat:Set player selection to player input in lower case

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //variable player input is equal to the inputted value in prompt with text “Choose your move”//
 const playerInput = prompt("Choose your move");
+
+//Set player selection to player input in lower case//
+const playerSelection = playerInput.toLowerCase();
