d023aa483eea166b1b18fc469935aa6802301dee
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-13 19:59:08 -0400

    feat:Add getComputerMove function

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const playerInput = prompt("Choose your move");
 
 //Set player selection to player input in lower case//
 const playerSelection = playerInput.toLowerCase();
+
+//Get computer move//
+function getComputerChoice() {
+    //Choose random number//
+    const randomNumber = Math.random();
+    //If random number is between 2/3 and 1, return scissors, if between ⅓ and ⅔, return paper, else, rock.//
+    if (randomNumber < 1 && randomNumber > 2/3) {
+        return "scissors";
+    }
+    else if (randomNumber < 2/3 && randomNumber > 1/3) {
+        return "paper";
+    }
+    else {
+        return "rock";
+    }
+}
