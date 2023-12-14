52a5353e43c27ab763ade747775405cc430de10e
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-13 20:05:48 -0400

    feat:Put player input and selection in startGame function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-//variable player input is equal to the inputted value in prompt with text “Choose your move”//
-const playerInput = prompt("Choose your move");
+function startGame() {
+    //variable player input is equal to the inputted value in prompt with text “Choose your move”//
+    const playerInput = prompt("Choose your move");
 
-//Set player selection to player input in lower case//
-const playerSelection = playerInput.toLowerCase();
+    //Set player selection to player input in lower case//
+    const playerSelection = playerInput.toLowerCase();
+}
 
 //Get computer move//
 function getComputerChoice() {
